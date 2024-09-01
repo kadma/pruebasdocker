dfeaea42232038a20047d2821a12cbe4add365e7
kadma <lolocruser@gmail.com>
2024-09-01 00:04:57 -0500

    Update script.sh

--- onedrive/engine/script.sh
+++ onedrive/engine/script.sh
@@ -1,4 +1,4 @@
 apt update && apt upgrade -y && apt autoremove -y && apt install python3 python3-pip -y && apt install git nano curl -y
-git https://github.com/TheCaduceus/Microsoft-E5-Auto-Renewal /app
+git clone https://github.com/TheCaduceus/Microsoft-E5-Auto-Renewal /app
 pip3 install -r /app/requirements.txt
 python3 /app/main.py
