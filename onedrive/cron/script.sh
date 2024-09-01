609cc65aad6001a672a0be10ff42186dcb626bdc
kadma <lolocruser@gmail.com>
2024-08-31 23:49:32 -0500

    Update script.sh
    agragado schedule

--- onedrive/cron/script.sh
+++ onedrive/cron/script.sh
@@ -1,3 +1,4 @@
 apt update && apt upgrade -y && apt autoremove -y && apt install python3 python3-pip -y && apt install git nano curl wget -y
+pip3 install schedule
 wget -b -P /app/cron1m.py https://raw.githubusercontent.com/kadma/pruebasdocker/main/onedrive/cron/cron1m.py
 wget -b -P /app/cron1m.py https://raw.githubusercontent.com/kadma/pruebasdocker/main/onedrive/cron/cron7h.py
