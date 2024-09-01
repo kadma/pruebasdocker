7be97613163b8f41c1c20a293305c3bfa54e18da
kadma <lolocruser@gmail.com>
2024-08-31 23:48:49 -0500

    Create script.sh
    scrip para ejecutar manual

---
+++ onedrive/cron/script.sh
@@ -0,0 +1,3 @@
+apt update && apt upgrade -y && apt autoremove -y && apt install python3 python3-pip -y && apt install git nano curl wget -y
+wget -b -P /app/cron1m.py https://raw.githubusercontent.com/kadma/pruebasdocker/main/onedrive/cron/cron1m.py
+wget -b -P /app/cron1m.py https://raw.githubusercontent.com/kadma/pruebasdocker/main/onedrive/cron/cron7h.py
